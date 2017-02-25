83a7fbc6748db3d7897d5e573601d92eb5b968cc
mallek <travis@haleycomputersolutions.com>
2017-02-24 17:56:18 -0700

    init

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('./config/config');
+
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+
+app.listen(port, () => {
+    console.log(`started on port ${port}`);
+});
+
+
+module.exports = { app };
