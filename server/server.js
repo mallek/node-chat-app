5f362761fe397928659120b0195c791924167af7
mallek <travis@haleycomputersolutions.com>
2017-02-26 13:24:42 -0700

    added location message

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message')
-const publicPath = path.join(__dirname, '../public')
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT;
 var app = express();
 var server = http.createServer(app);
@@ -17,14 +17,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcone to the chat app'))
+    socket.emit('newMessage', generateMessage('Admin', 'Welcone to the chat app'));
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New  user joined'));
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback('This is from the server');
+    });
+
+    socket.on('createLocationMessage', function (coords) {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message')
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', function () {
     it('should generate correct message object', () => {
@@ -8,7 +8,7 @@ describe('generateMessage', function () {
         var message = {
             from: 'TestUser',
             text: 'This is a test message'
-        }
+        };
 
         var sut = generateMessage(message.from, message.text);
         expect(sut.from).toBe(message.from);
@@ -19,5 +19,19 @@ describe('generateMessage', function () {
             from: message.from,
             text: message.text
         });
+    });
+});
+
+describe('generateLocationMessage', function () {
+    it('should generate correct location object', function () {
+        var from = "TestUser";
+        var lat = 1;
+        var long = 1;
+
+        var sut = generateLocationMessage(from, lat, long);
+        expect(sut.from).toBe(from);
+        expect(sut.url).toBe(`https://www.google.com/maps?q=1,1`);
+        expect(sut.createdAt).toBeA('number');
+
     });
 });
