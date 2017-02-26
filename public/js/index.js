25bf13452a1dad1147f91f33206ca9885b7c4ed1
mallek <travis@haleycomputersolutions.com>
2017-02-26 14:04:33 -0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -34,11 +34,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -49,12 +51,18 @@ locationButton.on('click', function () {
         return alert('Gerlocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
+
     }, function () {
         alert('unable to fetch location');
+        locationButton.removeAttr('disabled').text('Send location');
+
     });
 });
